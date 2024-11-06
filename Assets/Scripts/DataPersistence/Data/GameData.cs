c7a7d8e144d2ff20b211af2ab31cf4dcbf477776
DMossor81 <cmossor@students.kennesaw.edu>
2024-11-06 09:55:25 -0500

    Save System part 1

---
+++ Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    public int steps;
+    public Vector3 playerPosition;
+
+    public GameData()
+    {
+        steps = 0;
+        playerPosition = new Vector3(329.01f, -25.24f, 0f); //change to start position for full game (Starts in 1st forest room right now)
+    }
+}
