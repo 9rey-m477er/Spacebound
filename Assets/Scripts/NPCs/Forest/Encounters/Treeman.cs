1c5d35a5e6cd1b05ad5084dce7a160bf33b28c8b
DMossor81 <cmossor@students.kennesaw.edu>
2024-11-13 14:40:09 -0500

    Treeman

---
+++ Assets/Scripts/NPCs/Forest/Encounters/Treeman.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treeman : NPC, ITalkable, IBattleable
+{
+
+    [SerializeField] private DialogueText dialogueText;
+    [SerializeField] private DialogueController dialogueController;
+    [SerializeField] private EnemyStatSheet enemy;
+    [SerializeField] private AudioClip encounterIntro, encounterMusic;
+    [SerializeField] private GameObject bossSystem;
+    [SerializeField] private BossBattleUIScript bossScript;
+    private bool battleStarted = false;
+
+    public override void Interact()
+    {
+        Talk(dialogueText);
+        if (!battleStarted && dialogueController.paragraphs.Count == 0)
+        {
+            dialogueController.EndConversation();
+            Battle(enemy);
+        }
+    }
+
+    public void Talk(DialogueText dialogueText)
+    {
+        dialogueController.displayNextParagraph(dialogueText);
+    }
+
+    public void Battle(EnemyStatSheet enemy)
+    {
+        battleStarted = true;
+        bossSystem.SetActive(true);
+        bossScript.StartScriptedBattle(enemy, encounterIntro, encounterMusic);
+        this.gameObject.SetActive(false);
+    }
+}
