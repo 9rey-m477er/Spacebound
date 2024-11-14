a3003315ce7e8f24487214a67ef5d95505fa48c8
9rey-m477er <spangili@students.kennesaw.edu>
2024-11-14 00:27:47 -0500

    Added stuff to main menu buttons
    Hopefully I did that right, if not , we can revert this change and go back to the one with just the UI update

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    DataPersistenceManager dm;
+    sceneLoader sl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dm = GameObject.Find("DataPersistenceManager").GetComponent<DataPersistenceManager>();
+        sl = GameObject.Find("Canvas").GetComponent<sceneLoader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void NewGame()
+    {
+        dm.NewGame();
+        sl.LoadGame();
+    }
+    public void LoadGame()
+    {
+        dm.LoadGame();
+        sl.LoadGame();
+    }
+}
