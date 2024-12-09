baa55ac1f3f4c7072fddd9c7c2cba286f46778c6
DMossor81 <cmossor@students.kennesaw.edu>
2024-12-09 08:52:53 -0500

    Thozos and Bob Save Machines

---
+++ Assets/Scripts/NPCs/Forest/SaveMachines/SMForestBob.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SMForestBob : NPC, ITalkable, ISaveable
+{
+
+    [SerializeField] private DialogueText dialogueText;
+    [SerializeField] private DialogueController dialogueController;
+    public DataPersistenceManager dataPersistenceManager;
+    public override void Interact()
+    {
+        Talk(dialogueText);
+        SaveGame();
+    }
+
+    public void SaveGame()
+    {
+        dataPersistenceManager.SaveGame();
+    }
+
+    public void Talk(DialogueText dialogueText)
+    {
+        dialogueController.displayNextParagraph(dialogueText);
+    }
+}
