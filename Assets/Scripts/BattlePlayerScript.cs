8ba5070e1cc77a29145c379ebf285bff0f388f5e
DMossor81 <158850679+DMossor81@users.noreply.github.com>
2025-01-31 10:09:17 -0500

    CharSheet prepwork

--- Assets/Scripts/BattlePlayerScript.cs
+++ Assets/Scripts/BattlePlayerScript.cs
@@ -6,15 +6,7 @@ public class BattlePlayerScript : MonoBehaviour
 {
     public float health = 0;
     public float startingHealth = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float slashMultiplier = 0;
+    public float bashMultiplier = 0;
+    public float pokeMultiplier = 0;
 }
--- Assets/Scripts/CharacterStatSheet.cs
+++ Assets/Scripts/CharacterStatSheet.cs
@@ -10,7 +10,7 @@ public class CharacterStatSheet : ScriptableObject
     public Sprite characterSprite = null;
     public Sprite attackSprite = null;
     public int characterLevel = 1;
-    public int characterHP = 30;
+    public float characterHP = 30;
     public float bashMultiplier = 1.0f;
     public float slashMultiplier = 1.0f;
     public float pokeMultiplier = 1.0f;
