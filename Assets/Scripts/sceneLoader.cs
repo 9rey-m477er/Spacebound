d05c01801c202dae520788b3b07a560687c28003
conartst <159001421+conartst@users.noreply.github.com>
2025-04-15 14:26:34 -0400

    update

--- Assets/Scripts/sceneLoader.cs
+++ Assets/Scripts/sceneLoader.cs
@@ -8,6 +8,8 @@ public class sceneLoader : MonoBehaviour
 {
     public Image fadeImage;
     public float fadeDuration = 0.3f;
+    public float fadeDuration2 = 0.3f;
+
     private void Start()
     {
         fadeImage.gameObject.SetActive(false);
@@ -26,10 +28,10 @@ public class sceneLoader : MonoBehaviour
     public IEnumerator goToMain()
     {
         fadeImage.gameObject.SetActive(true);
-        yield return StartCoroutine(Fade(1));
+        yield return StartCoroutine(Fade2(1));
         Debug.Log("Loading Main Menu");
         SceneManager.LoadScene("Main Menu");
-        yield return StartCoroutine(Fade(0));
+        yield return StartCoroutine(Fade2(0));
     }
     public void MainMenu()
     {
@@ -45,6 +47,23 @@ public class sceneLoader : MonoBehaviour
         {
             elapsedTime += Time.deltaTime;
             float newAlpha = Mathf.Lerp(startAlpha, targetAlpha, elapsedTime / fadeDuration);
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, newAlpha);
+            yield return null;
+        }
+
+        // Ensure the final alpha value is set
+        fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, targetAlpha);
+    }
+
+    private IEnumerator Fade2(float targetAlpha)
+    {
+        float startAlpha = fadeImage.color.a;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration2)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startAlpha, targetAlpha, elapsedTime / fadeDuration2);
             fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, newAlpha);
             yield return null;
         }
