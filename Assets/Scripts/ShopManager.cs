c2cbdb5ee14032c5fc0628d0af7171ebdc8204fd
DMossor81 <158850679+DMossor81@users.noreply.github.com>
2025-04-24 22:31:35 -0400

    Shop Fix

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -51,6 +51,7 @@ public class ShopManager : MonoBehaviour
                 }
                 else
                 {
+                    isShopOpen = false;
                     CloseShop();
                 }
             }
